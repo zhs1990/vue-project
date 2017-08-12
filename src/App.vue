6f3c45ea1bf5441ca7aaf964152ba258e6d43415
[zhs1990] <[529954781@qq.com]>
2017-08-12 19:01:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <foot></foot>  
+  </div>
+</template>
+
+<script>
+import foot from './components/foot.vue'
+export default {
+  components: {
+    foot
+  }
+}
+</script>
+
+<style>
+  *{
+    margin:0;
+    padding: 0;
+    list-style: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  
+</style>
---
+++ src/components/foot.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/icon_nav_button.png">
+        <span slot="label">重点监测</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/icon_nav_msg.png">
+        <span slot="label">舆情分析</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="../assets/icon_nav_article.png">
+        <span slot="label">高级搜索</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/icon_nav_cell.png">
+        <span slot="label">个人中心</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+export default {
+  components: {
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
